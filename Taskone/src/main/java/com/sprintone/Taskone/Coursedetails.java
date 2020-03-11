420986813212da08658ca0372caa4e43ccebc28d
HarishParthasarathy <59193847+HarishParthasarathy@users.noreply.github.com>
2020-03-11 10:47:44 +0530

    Spring Boot Mapping Example

---
+++ Taskone/src/main/java/com/sprintone/Taskone/Coursedetails.java
@@ -0,0 +1,37 @@
+package com.sprintone.Taskone;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Courses")
+public class Coursedetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cid;
+	
+	private String cname;
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+}
---
+++ Taskone/src/main/java/com/sprintone/Taskone/TaskoneApplication.java
@@ -0,0 +1,16 @@
+package com.sprintone.Taskone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author harish.parthasarathy
+ *
+ */
+@SpringBootApplication
+public class TaskoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskoneApplication.class, args);
+	}
+}
