05bd34f44c48c8f10c08c6684c4e44945627ff7b
HarishParthasarathy <59193847+HarishParthasarathy@users.noreply.github.com>
2020-03-17 17:02:11 +0530

    Commit All

--- Angular_with_SpringBoot/Taskfirst/src/main/java/com/sprintone/Taskone/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprintone.Taskone;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-	
-}
