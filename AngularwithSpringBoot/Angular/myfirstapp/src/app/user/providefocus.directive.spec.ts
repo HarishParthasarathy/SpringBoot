b3f8e24a6d0c0a61b8e64189964e6ad1c601d275
HarishParthasarathy <59193847+HarishParthasarathy@users.noreply.github.com>
2020-03-17 17:29:26 +0530

    Delete

--- AngularwithSpringBoot/Angular/myfirstapp/src/app/user/providefocus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProvidefocusDirective } from './providefocus.directive';
-
-describe('ProvidefocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ProvidefocusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
